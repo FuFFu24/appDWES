01ea4a513e4c2681215e6175663d7b76e2daced1
FuFFu24 <floalonso.abad@gmail.com>
2023-10-16 09:11:00 +0200

    16/10 saneamiento

--- ENUNCIADOS Recogida sanear validar/ej2.php
+++ ENUNCIADOS Recogida sanear validar/ej2.php
@@ -1,31 +1,82 @@
 <?php
-// Función para sanear los datos en un vector asociativo
-function sanitizeArray($data, $default = '') {
-    foreach ($data as $key => $value) {
-        // Sanea el valor y asigna el valor predeterminado si está vacío o no definido
-        $data[$key] = isset($value) ? filter_var($value, FILTER_SANITIZE_STRING) : $default;
-    }
-    return $data;
-}
 
-// Vector con datos (pueden ser datos del formulario, por ejemplo)
-$data = [
-    'nombre' => $_POST['nombre'] ?? '', // Valor predeterminado vacío
-    'telefono' => $_POST['telefono'] ?? '', // Valor predeterminado vacío
-    'email' => $_POST['email'] ?? '', // Valor predeterminado vacío
-    'direccion' => $_POST['direccion'] ?? '', // Valor predeterminado vacío
-];
+    $texto = $_REQUEST['Nombre'];
+print "<p>";
+        if(isset($texto) && !is_numeric($texto) && !empty($texto)){
+            print 'El nombre es: '.htmlspecialchars(trim(strip_tags($texto)),ENT_QUOTES,"utf-8");
+        }else{
+            print "<p>El nombre introducido es incorrecto o esta vacio</p>";
+        }
+        print "</p>";
+        $contraseña = $_REQUEST['Contraseña'];
+print "<p>";
+        if(!mb_strlen($contraseña) < 5){
+            print 'La contraseña es: '.htmlspecialchars(trim(strip_tags($contraseña)),ENT_QUOTES,"utf-8");
+        }else{
+            print "Los caracteres de la cadena son menores de 5";
+        }
+print "</p>";
+        $estudios = $_REQUEST['Educacion'];
+        print "<p>";
+        switch ($estudios) {
+            case $estudios == "Sin estudios":
+                print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+                break;
 
-// Llama a la función para sanear el vector de datos
-$sanitizedData = sanitizeArray($data);
+            case $estudios == "ESO";
+            print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+            break;
+            case $estudios == "Bachillerato":
+                print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+                break;
 
-// Los datos saneados ahora se encuentran en $sanitizedData
-// Puedes acceder a ellos de esta manera:
-$nombre = $sanitizedData['nombre'];
-$telefono = $sanitizedData['telefono'];
-$email = $sanitizedData['email'];
-$direccion = $sanitizedData['direccion'];
+            case $estudios == "FP";
+            print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+            break;
 
-// Puedes usar los valores saneados como sea necesario
-// Por ejemplo, imprimirlos o almacenarlos en una base de datos
+            case $estudios == "Universitario":
+                print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+                break;
+
+            case $estudios == "Otros";
+            print htmlspecialchars(trim(strip_tags($estudios)),ENT_QUOTES,"utf-8");
+            break;                                    
+            default:
+                print htmlspecialchars(trim(strip_tags("No tiene estudios")),ENT_QUOTES,"utf-8");
+                break;
+        }
+        print "</p>";
+
+        if(isset($_REQUEST['Nacionalidad']) =="Hispana"){
+            print htmlspecialchars(trim(strip_tags($_REQUEST['Nacionalidad'])),ENT_QUOTES,"utf-8");
+        }else if(isset($_REQUEST['Nacionalidad']) =="Sajona"){
+            print htmlspecialchars(trim(strip_tags($_REQUEST['Nacionalidad'])),ENT_QUOTES,"utf-8");
+        }else if(isset($_REQUEST['Nacionalidad']) =="Otro"){
+            print htmlspecialchars(trim(strip_tags($_REQUEST['Nacionalidad'])),ENT_QUOTES,"utf-8");
+        }
+
+        if(isset($_REQUEST['Ingles']) == "Ingles"){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['Ingles'])),ENT_QUOTES,"utf-8")."</p>";
+        }
+        if(isset($_REQUEST['Castellano']) == "Castellano"){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['Castellano'])),ENT_QUOTES,"utf-8")."</p>";
+        }
+        if(isset($_REQUEST['Frances']) == "Frances"){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['Frances'])),ENT_QUOTES,"utf-8")."</p>";
+        }
+        if(isset($_REQUEST['Aleman']) == "Aleman"){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['Aleman'])),ENT_QUOTES,"utf-8")."</p>";
+        }
+
+        if(preg_match('/^[a-zA-Z0-9.*%]+@gmail\.com$/',$_REQUEST['email'])){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['email'])),ENT_QUOTES,"utf-8")."</p>";
+        }else{
+            print "El email no esta con caracteres aceptados";
+        }
+        if(preg_match('/^www\/\/www\[a-zA-Z0-9]\.com$/',$_REQUEST['url'])){
+            print "<p>".htmlspecialchars(trim(strip_tags($_REQUEST['url'])),ENT_QUOTES,"utf-8")."</p>";
+        }else{
+        print "<p>La url no es aceptada</p>";
+        }
 ?>
+<p><a href="ej2.html">Volver al formulario</a></p>
--- ENUNCIADOS Recogida sanear validar/ej3.php
+++ ENUNCIADOS Recogida sanear validar/ej3.php
@@ -1,97 +1,62 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Formulario de Datos</title>
-</head>
-<body>
-    <h1>Formulario de Datos</h1>
-    <form method="post" action="">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre"><br>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password"><br>
-
-        <label for="educacion">Educación:</label>
-        <select name="educacion">
-            <option value="Sin estudios">Sin estudios</option>
-            <option value="Eso">Eso</option>
-            <option value="Bachillerato">Bachillerato</option>
-            <option value="F.P.">F.P.</option>
-            <option value="Universitario">Universitario</option>
-            <option value="Otros">Otros</option>
-        </select><br>
-
-        <label>Nacionalidad:</label><br>
-        <input type="radio" name="nacionalidad" value="Hispana"> Hispana<br>
-        <input type="radio" name="nacionalidad" value="Sajona"> Sajona<br>
-        <input type="radio" name="nacionalidad" value="Otras"> Otras<br>
-
-        <label>Idiomas:</label><br>
-        <input type="checkbox" name="idiomas[]" value="Inglés"> Inglés<br>
-        <input type="checkbox" name="idiomas[]" value="Castellano"> Castellano<br>
-        <input type="checkbox" name="idiomas[]" value="Francés"> Francés<br>
-        <input type="checkbox" name="idiomas[]" value="Alemán"> Alemán<br>
-
-        <label for="email">Email:</label>
-        <input type="text" name="email"><br>
-
-        <label for="website">Sitio web:</label>
-        <input type="text" name="website"><br>
-
-        <input type="submit" name="submit" value="Enviar">
-    </form>
-
-    <?php
-    if (isset($_POST['submit'])) {
-        // Realiza la validación y procesamiento de datos aquí
-        $errors = array();
-
-        $nombre = $_POST['nombre'];
-        $password = $_POST['password'];
-        $educacion = isset($_POST['educacion']) ? $_POST['educacion'] : array();
-        $nacionalidad = $_POST['nacionalidad'];
-        $idiomas = isset($_POST['idiomas']) ? $_POST['idiomas'] : array();
-        $email = $_POST['email'];
-        $website = $_POST['website'];
-
-        if (empty($nombre)) {
-            $errors[] = 'El campo Nombre es obligatorio.';
-        }
-
-        if (strlen($password) < 5) {
-            $errors[] = 'La contraseña debe tener al menos 5 caracteres.';
-        }
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'El formato del correo electrónico no es válido.';
-        }
-
-        if (!filter_var($website, FILTER_VALIDATE_URL)) {
-            $errors[] = 'El formato del sitio web no es válido.';
-        }
-
-        if (empty($errors)) {
-            echo '<h2>Datos introducidos por el usuario:</h2>';
-            echo 'Nombre: ' . $nombre . '<br>';
-            echo 'Contraseña: ' . $password . '<br>';
-            if (!empty($educacion) && is_array($educacion)) {
-                echo 'Educación: ' . implode(', ', $educacion) . '<br>';
-            } else {
-                echo 'Educación: No se ha seleccionado ninguna opción.<br>';
-            }            
-            echo 'Nacionalidad: ' . $nacionalidad . '<br>';
-            echo 'Idiomas: ' . implode(', ', $idiomas) . '<br>';
-            echo 'Correo electrónico: ' . $email . '<br>';
-            echo 'Sitio web: ' . $website . '<br>';
-        } else {
-            echo '<div style="color: red;">';
-            foreach ($errors as $error) {
-                echo $error . '<br>';
-            }
-            echo '</div>';
-        }
+<?php
+        print "<p>Estos son los datos introducidos</p>";
+    print "<ul>";
+    if(!empty($_REQUEST['textoBusqueda'])){
+        print "<li>Texto de busqueda ". $_REQUEST['textoBusqueda']."</li>";
+    }else{
+        print "<li>No ha introducido nada</li>";
     }
-    ?>
-</body>
-</html>
+
+    if(!empty($_REQUEST['Canciones']) == "Titulos de la cancion"){
+        print "<li>Buscar por ".htmlspecialchars(trim(strip_tags($_REQUEST['Canciones'])),ENT_QUOTES,"utf-8")."</li>";
+    }else if(!empty($_REQUEST['Canciones']) == "Nombres de album"){
+        print "<li>Buscar por ".htmlspecialchars(trim(strip_tags($_REQUEST['Canciones'])),ENT_QUOTES,"utf-8")."</li>";
+    }else if(!empty($_REQUEST['Canciones']) == "Ambos Campos"){
+        print "<li>Buscar por".htmlspecialchars(trim(strip_tags($_REQUEST['Canciones'])),ENT_QUOTES,"utf-8")."</li>";
+    }else{
+        print "<li>No ha seleccionado ningun radio boton</li>";
+    }
+    print "<li>";
+
+        switch ($_REQUEST['Genero musical']) {
+            case ($_REQUEST['Genero musical'] == "Todos"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Acustica"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Banda sonora"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Blues"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Electronica"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Folk"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Jazz"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "New Age"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Pop"):
+                print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                break;
+            case ($_REQUEST['Genero musical'] == "Rock"):
+            print "Genero: ".htmlspecialchars(trim(strip_tags($_REQUEST['Genero musical'])),ENT_QUOTES,"utf-8");
+                 break;
+            default:
+                print "No le gusta nada";
+                break;
+        }
+
+    print "</li>";
+
+    print "</ul>";
+
+?>
+<p><a href="ej3.html">Volver al formulario</a></p>
