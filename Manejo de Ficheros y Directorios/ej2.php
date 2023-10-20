60adec3ca40b0b8d8282874a84d829275bf1f082
FuFFu24 <floalonso.abad@gmail.com>
2023-10-20 10:27:47 +0200

    20/10

---
+++ Manejo de Ficheros y Directorios/ej2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $fcontador = fopen("contador.txt", "r+");
+    $fdatos = fgets($fcontador);
+    // rewind($fcontador);
+    fseek($fcontador, 0);
+    fwrite($fcontador, $suma);
+    fclose($fcontador);
+    ?>
+</body>
+</html>
