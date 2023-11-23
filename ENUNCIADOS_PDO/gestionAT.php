cba6f4c1401b51e1e68647d6868f2bacdfa93f78
FuFFu24 <floalonso.abad@gmail.com>
2023-11-23 10:45:48 +0100

    23/11 PDO

---
+++ ENUNCIADOS_PDO/gestionAT.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Base de datos PDO Bloque 2: aplicacion de gestion de una bbdd</h1>
+    <ul>
+        <li>
+            <a href="insertar_1.php">Insertar registros</a>
+        </li>
+        <li>
+            <a href="Listar.php">Listado</a>
+        </li>
+        <li>
+            <a href="borrar_1.php">Borrar un Registro</a>
+        </li>
+    </ul>
+</body>
+</html>
